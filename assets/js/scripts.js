9aa9aa65f4e5ed72b0d8d59223930b84bb00b538
Shane-Bath <shane.bath@gmail.com>
2023-01-25 16:14:32 +0000

    add font to HTML and CSS

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,1 +1,24 @@
-console.log('Hello world')
+//Collection of game cards place on the board
+
+let choiceArray = [
+    { 
+        name: 'pic-1',
+        img:  'assets/images/pic-1'
+    },
+]
+
+// Create game area 
+
+//Shuffle game cards 
+
+//check for matches 
+
+// Game difficulty 
+
+// call to action - eventlistener
+
+//timer
+
+// count guesses and record guesses
+
+//reset board
