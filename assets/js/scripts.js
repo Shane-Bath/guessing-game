46e0abaabad8162897027298a4f52e2505f1a008
Shane-Bath <shane.bath@gmail.com>
2023-01-25 17:24:06 +0000

    Add images and game board structure in js and css

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,13 +1,125 @@
-//Collection of game cards place on the board
+document.addEventListener('DOMContentLoaded', () => {
+
+
+//Collection of game cards to place on the board
 
 let choiceArray = [
     { 
         name: 'pic-1',
         img:  'assets/images/pic-1'
     },
-]
+    { 
+        name: 'pic-1',
+        img:  'assets/images/pic-1'
+    },
+    { 
+        name: 'pic-2',
+        img:  'assets/images/pic-2'
+    },
+    { 
+        name: 'pic-2',
+        img:  'assets/images/pic-2'
+    },
+    { 
+        name: 'pic-3',
+        img:  'assets/images/pic-3'
+    },
+    { 
+        name: 'pic-4',
+        img:  'assets/images/pic-4'
+    },
+    { 
+        name: 'pic-4',
+        img:  'assets/images/pic-4'
+    },
+    { 
+        name: 'pic-5',
+        img:  'assets/images/pic-5'
+    },
+    { 
+        name: 'pic-5',
+        img:  'assets/images/pic-5'
+    },
+    { 
+        name: 'pic-6',
+        img:  'assets/images/pic-6'
+    },
+    { 
+        name: 'pic-6',
+        img:  'assets/images/pic-6'
+    },
+    { 
+        name: 'pic-7',
+        img:  'assets/images/pic-7'
+    },
+    { 
+        name: 'pic-7',
+        img:  'assets/images/pic-7'
+    },
+    { 
+        name: 'pic-8',
+        img:  'assets/images/pic-8'
+    },
+    { 
+        name: 'pic-8',
+        img:  'assets/images/pic-8'
+    },
+    { 
+        name: 'pic-9',
+        img:  'assets/images/pic-9'
+    },
+    { 
+        name: 'pic-9',
+        img:  'assets/images/pic-9'
+    },
+    { 
+        name: 'pic-10',
+        img:  'assets/images/pic-10'
+    },
+    { 
+        name: 'pic-10',
+        img:  'assets/images/pic-10'
+    },
+    { 
+        name: 'pic-10',
+        img:  'assets/images/pic-10'
+    },
+    { 
+        name: 'pic-11',
+        img:  'assets/images/pic-11'
+    },
+    { 
+        name: 'pic-11',
+        img:  'assets/images/pic-11'
+    },
+    { 
+        name: 'pic-12',
+        img:  'assets/images/pic-12'
+    },
+    { 
+        name: 'pic-12',
+        img:  'assets/images/pic-12'
+    },
 
-// Create game area 
+
+]
+// Create the game area 
+
+const board = document.querySelector('.board');
+
+function gameArea () {
+    for (let i = 0; i < choiceArray.length; i++) {
+        var card = document.createElement('img');
+        card.setAttribute('src','/assets/images/foreground.png');
+        card.setAttribute('data-id', i);
+        board.appendChild(card);
+
+    }
+}
+
+gameArea()
+
+
 
 //Shuffle game cards 
 
@@ -22,3 +134,5 @@ let choiceArray = [
 // count guesses and record guesses
 
 //reset board
+
+})
